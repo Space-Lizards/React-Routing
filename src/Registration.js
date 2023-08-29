3bb63279ce8d260c02518aaa29c3a16590baf92f
Space-Mushroom <ait.creator@mail.ru>
2023-08-29 17:23:16 +0300

    fixes bugs

--- src/Registration.js
+++ src/Registration.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Registration() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
 
   const handleRegistration = () => {
     // Здесь вы можете добавить логику для регистрации пользователя
@@ -18,19 +17,19 @@ function Registration() {
       <form onSubmit={handleRegistration}>
         <div>
           <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
         </div>
         <div>
           <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
         </div>
         <button type="submit">Зарегистрироваться</button>
       </form>
